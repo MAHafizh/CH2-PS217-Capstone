8cedab7facd8a5e3af99aaf7ce1790cbe8247d6a
MAHafizh <hafizh12jr@gmail.com>
2023-12-03 17:15:35 +0700

    update from cc, added storage api not completed

--- src/controller.js
+++ src/controller.js
@@ -1,195 +1,189 @@
-const {userRefs, storage} = require('../firebase/config');
+const { userRefs } = require("../firebase/configFirestore");
 
 const index = (req, res) => {
-    res.status(200).json({
-        status: {
-            code: 200,
-            message: "Success fetching the API",
-        },
-        data: null,
-    });
+  res.status(200).json({
+    status: {
+      code: 200,
+      message: "Success fetching the API",
+    },
+    data: null,
+  });
 };
 
 const getUsers = async (req, res) => {
+  const id = req.params.id || req.query.id;
 
-    const id = req.params.id || req.query.id;
+  if (id) {
+    try {
+      const snapshot = await userRefs.doc(id).get();
 
-    if(id) {
+      if (!snapshot.exists) {
+        return res.status(404).json({
+          status: {
+            code: 404,
+            message: "User Not Found",
+          },
+          data: null,
+        });
+      }
 
-        try{
-            const snapshot = await userRefs.doc(id).get()
-    
-            if(!snapshot.exists) {
-                return res.status(404).json({
-                    status: {
-                        code: 404,
-                        message: "User Not Found"
-                    },
-                    data: null
-                })
-            }
-    
-            const list = { id: snapshot.id, ...snapshot.data() };
-    
-            res.status(200).json({
-                status: {
-                    code: 200,
-                    message: "Success"
-                },
-                data: list
-            });
-        } catch(error) {
-            console.error(error);
-            res.status(500).json({
-                status: {
-                    code: 500,
-                    message: "Internal Server Error"
-                },
-                error: error.message
-            });
-        }
-    } else {
+      const list = { id: snapshot.id, ...snapshot.data() };
 
-        try {
-            const snapshot = await userRefs.get();
-            const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-            const count = snapshot.size;
-            
-            res.status(200).json({
-                status: {
-                    code: 200,
-                    message: "Success"
-                },
-                total: count,
-                data: list
-            });
-        } catch(error) {
-            console.error(error);
-            res.status(500).json({
-                status: {
-                    code: 500,
-                    message: "Internal Server Error"
-                },
-                error: error.message
-            });
-        }
+      res.status(200).json({
+        status: {
+          code: 200,
+          message: "Success",
+        },
+        data: list,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        status: {
+          code: 500,
+          message: "Internal Server Error",
+        },
+        error: error.message,
+      });
     }
-}
+  } else {
+    try {
+      const snapshot = await userRefs.get();
+      const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      const count = snapshot.size;
+
+      res.status(200).json({
+        status: {
+          code: 200,
+          message: "Success",
+        },
+        total: count,
+        data: list,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        status: {
+          code: 500,
+          message: "Internal Server Error",
+        },
+        error: error.message,
+      });
+    }
+  }
+};
 
 const addUser = async (req, res) => {
-    try{
-        const data = req.body;
-        await userRefs.add(data);
-        res.status(200).json({
-            status: {
-                code: 200,
-                message: "User Added"
-            },
-            data: data
-        });
+  try {
+    const data = req.body;
+    await userRefs.add(data);
+    res.status(200).json({
+      status: {
+        code: 200,
+        message: "User Added",
+      },
+      data: data,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: {
+        code: 500,
+        message: "Internal Server Error",
+      },
+      error: error.message,
+    });
+  }
+};
 
-    } catch(error) {
-        console.error(error);
-        res.status(500).json({
-            status: {
-                code: 500,
-                message: "Internal Server Error"
-            },
-            error: error.message
-        });
-    }
-}
+// const addImgUser = async (req, res) => {
+//     try{
+//         const id = req.params.id || req.query.id
+//         const file = req.uploadedFile;
 
-const addImgUser = async (req, res) => {
-    try{
-        const id = req.params.id || req.query.id
-        const file = req.files.file
+//         const bucket = storage.bucket('testing-project-92c7c.appspot.com');
+//         const fileUpload = bucket.file(`userImages/${id}/${file.name}`);
+//         await fileUpload.save(file.data);
 
-        const bucket = storage.bucket('testing-project-92c7c.appspot.com');
-        const fileUpload = bucket.file(`userImages/${id}/${file.name}`);
-        await fileUpload.save(file.data);
+//         const [url] = await fileUpload.getSignedUrl({
+//             action: 'read',
+//             expires: '12-12-2024'
+//         });
 
-        const [url] = await fileUpload.getSignedUrl({
-            action: 'read',
-            expires: '12-12-2024'
-        });
+//         await userRefs.doc(id).update({
+//             userimage: url
+//         });
 
-        await userRefs.doc(id).update({
-            userimage: url
-        });
-
-        res.status(200).json({
-            status: {
-                code: 200,
-                message: "Images Added"
-            },
-            data: url
-        });
-    } catch(error) {
-        console.error(error);
-        res.status(500).json({
-            status: {
-                code: 500,
-                message: "Internal Server Error"
-            },
-            error: error.message
-        });
-    }
-}
+//         res.status(200).json({
+//             status: {
+//                 code: 200,
+//                 message: "Images Added"
+//             },
+//             data: url
+//         });
+//     } catch(error) {
+//         console.error(error);
+//         res.status(500).json({
+//             status: {
+//                 code: 500,
+//                 message: "Internal Server Error"
+//             },
+//             error: error.message
+//         });
+//     }
+// }
 
 const updateUser = async (req, res) => {
-    try {
-        const id = req.params.id || req.query.id;
-        const data = req.body;
-        await userRefs.doc(id).update(data);
-        res.status(200).json({
-            status: {
-                code: 200,
-                message: "User Updated"
-            },
-            data: data
-        });
-
-    } catch(error) {
-        console.error(error);
-        res.status(500).json({
-            status: {
-                code: 500,
-                message: "Internal Server Error"
-            },
-            error: error.message
-        });
-    }
-}
+  try {
+    const id = req.params.id || req.query.id;
+    const data = req.body;
+    await userRefs.doc(id).update(data);
+    res.status(200).json({
+      status: {
+        code: 200,
+        message: "User Updated",
+      },
+      data: data,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: {
+        code: 500,
+        message: "Internal Server Error",
+      },
+      error: error.message,
+    });
+  }
+};
 
 const deleteUser = async (req, res) => {
-    try {
-        const id = req.params.id || req.query.id;
-        await userRefs.doc(id).delete();
-        res.status(200).json({
-            status: {
-                code: 200,
-                message: "User Deleted"
-            }
-        });
-
-    } catch(error) {
-        console.error(error);
-        res.status(500).json({
-            status: {
-                code: 500,
-                message: "Internal Server Error"
-            },
-            error: error.message
-        });
-    }
-}
+  try {
+    const id = req.params.id || req.query.id;
+    await userRefs.doc(id).delete();
+    res.status(200).json({
+      status: {
+        code: 200,
+        message: "User Deleted",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: {
+        code: 500,
+        message: "Internal Server Error",
+      },
+      error: error.message,
+    });
+  }
+};
 
 module.exports = {
-    index,
-    getUsers,
-    addUser,
-    updateUser,
-    deleteUser,
-    addImgUser
+  index,
+  getUsers,
+  addUser,
+  updateUser,
+  deleteUser,
+  //addImgUser,
 };
--- src/router.js
+++ src/router.js
@@ -1,6 +1,96 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
 const router = express.Router();
-const { index, getUsers, addUser, updateUser, deleteUser, addImgUser } = require("./controller.js");
+const { userRefs } = require("../firebase/configFirestore.js");
+const { bucket: storageBucket } = require('../firebase/configStorage.js');
+
+const {
+  index,
+  getUsers,
+  addUser,
+  updateUser,
+  deleteUser,
+} = require("./controller.js");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const dir = "images";
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const filter = function (req, file, cb) {
+  if (
+    file.mimetype == "image/png" ||
+    file.mimetype == "image/jpeg" ||
+    file.mimetype == "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    req.errorMessage = "File is not a valid image!";
+    cb(null, false);
+  }
+};
+
+const upload = multer({ storage, filter });
+
+router.post("/upload/", upload.single("image"), async (req, res) => {
+  try {
+    id = req.query.id || req.params.id
+
+    if (!id) {
+        return res.status(400).json({
+          status: {
+            code: 400,
+            message: "Bad Request",
+          },
+          error: "Missing 'id' parameter.",
+        });
+      }
+
+    const file = req.file;
+
+    const bucket = storageBucket;
+    const timestamp = new Date().toISOString();
+    const filePath = `images/${timestamp}_${file.originalname}`;
+    const uploadOpt = {
+      destination: filePath,
+      metadata: { contentType: file.mimetype },
+    };
+    await bucket.upload(file.path, uploadOpt);
+
+    const imageURL = `https://storage.googleapis.com/${bucket.name}/${filePath}`;
+
+    await userRefs.doc(id).update({ imageURL });
+
+    res.status(200).json({
+      status: {
+        code: 200,
+        message: "Images Added",
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: {
+        code: 500,
+        message: "Internal Server Error",
+      },
+      error: error.message,
+    });
+  }
+});
 
 router.get("/", index);
 router.get("/users", getUsers);
@@ -10,7 +100,5 @@ router.put("/users", updateUser);
 router.put("/users/:id", updateUser);
 router.delete("/users", deleteUser);
 router.delete("/users/:id", deleteUser);
-router.post("/users/images", addImgUser);
-router.post("/users/images/:id", addImgUser);
 
 module.exports = router;
